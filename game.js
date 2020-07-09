af5c001ddbc3235cedf417d6c6e8ba88a4715678
kkellerman <keithrk1@gmail.com>
2020-07-08 23:48:35 -0400

    continue with main function

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
 //setup game loop
+let lastRenderTime= 0;
+const SNAKE_SPEED = 2  //moves 2x per second
+
 function main(currentTime) {
-    window.requestAnimationFrame(main)
-    console.log(currentTime)
+   window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) /1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
+    
+    console.log('Render')
+    lastRenderTime = currentTime
 }
 
 window.requestAnimationFrame(main)
