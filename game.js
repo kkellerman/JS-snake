5be4e0709e8db9e3380a932d0aa92c1fae3a72b5
kkellerman <keithrk1@gmail.com>
2020-07-11 23:38:23 -0400

    pass gameBoard into function

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
 
 //setup game loop
 let lastRenderTime= 0;
-
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
    window.requestAnimationFrame(main)
@@ -28,5 +28,5 @@ function update() {
 }
 
 function draw(){
-    drawSnake()
+    drawSnake(gameBoard)
 }
