66642e1c6103b540453e564e4b2416a4d1249938
kkellerman <keithrk1@gmail.com>
2020-08-08 23:20:12 -0400

    import onSnake and expandSnake fomr snake.js file

--- snake.js
+++ snake.js
@@ -35,3 +35,7 @@ return snakeBody.some(segments =>{
   return equalPositions(segment, position)
   })
 }
+
+function equalPositions(pos1, pos2) {
+  return  pos1.x === pos2.x && pos1.y === pos2.y
+}
