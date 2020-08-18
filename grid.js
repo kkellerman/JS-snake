6a0920646d7e00e36d8bb0918643c14f740001c7
kkellerman <keithrk1@gmail.com>
2020-08-18 00:04:36 -0400

    add outsideGrid conditions

--- grid.js
+++ grid.js
@@ -5,8 +5,13 @@ export function RandomGridPosition() {
 return {
 x: Math.floor(Math.random()* GRID_SIZE) + 1,
 y: Math.floor(Math.random()* GRID_SIZE) + 1
-
+    }
 }
 
-
+export function outsideGrid(position) {
+    return (
+        position.x < 1 || position.x > GRID_SIZE ||
+        position.y < 1 || position.y > GRID_SIZE
+    )
 }
+
