50a5d3c8273a19148b34749644aab0ab40ac7bab
kkellerman <keithrk1@gmail.com>
2020-09-05 23:40:05 -0400

    import the latest functions into game.js

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
-import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead,
+snakeIntersection} from './snake.js'
  
  import {update as updateFood, draw as drawFood } from './food.js'
+import {outsideGrid } from './grid.js'
+
 
 //setup game loop
 let lastRenderTime= 0
