1b4f0231abe321c983d12619879dc568542422d2
kkellerman <keithrk1@gmail.com>
2020-09-05 23:37:20 -0400

    update snake position

--- snake.js
+++ snake.js
@@ -32,15 +32,19 @@ export function expandSnake(amount) {
  newSegments += amount
 }
 
-export function onSnake(position) {
-return snakeBody.some(segments =>{
+export function onSnake(position, { ignoreHead = false } = {}) {
+return snakeBody.some((segments, index) =>{
+  if (ignoreHead && index === 0) return false
   return equalPositions(segment, position)
   })
 }
 
 export function getSnakeHead() {
-    return snakeBody[0]
-    
+    return snakeBody[0]  
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignore: true } )
 }
 
 function equalPositions(pos1, pos2) {
