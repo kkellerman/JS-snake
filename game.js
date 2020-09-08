15488388bd1342392361c1632697f43334cf684c
kkellerman <keithrk1@gmail.com>
2020-09-07 23:44:52 -0400

    add window location

--- game.js
+++ game.js
@@ -12,7 +12,9 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
 if (gameOver){
-    if (confirm('You lost.  Press ok to restart.'))
+    if (confirm('You lost.  Press ok to restart.')) {
+        window.location = '/'
+    }
    return  alert('you lose')
 }
 
